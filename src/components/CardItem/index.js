f068f30433ca6443f555c62d59d788c5d9292f6f
1Pavan8 <142237970+1Pavan8@users.noreply.github.com>
2023-09-05 15:26:13 +0530

    Update index.js

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -9,7 +9,7 @@ const CardItem = props => {
     <li className={`card ${className}`}>
       <h1 className="title">{title}</h1>
       <p className="desc">{description}</p>
-      <img className="img" src={imgUrl} alt="avatar" />
+      <img className="img" src={imgUrl} alt={title} />
     </li>
   )
 }
